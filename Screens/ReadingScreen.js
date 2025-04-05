37ae0dc179d832a02b51905def0244ce18ae14d7
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2025-04-05 19:40:09 +0530

    Complete debugging

--- Screens/ReadingScreen.js
+++ Screens/ReadingScreen.js
@@ -1,108 +1,123 @@
 import React from 'react';
-import {Text,View,StyleSheet,TouchableOpacity,Image,TextInput} from 'react-native';
-import {BarCodeScanner} from 'expo-barcode-scanner';
-import * as Permissions from 'expo-permissions';
-export default class ReadingScreen extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            hasCameraPermissions:null,
-            scanned:false,
-            scannedBookId:'',
-            scannedStudentId:'',
-            buttonState:'normal'
+import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-        }
+export default class ReadingScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedBookId: '',
+      scannedStudentId: '',
+      buttonState: 'normal',
+    };
+  }
+
+  getCameraPermissions = async (id) => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    this.setState({
+      hasCameraPermissions: status === 'granted',
+      buttonState: id,
+      scanned: false,
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    const { buttonState } = this.state;
+
+    if (buttonState === 'bookId') {
+      this.setState({
+        scanned: true,
+        buttonState: 'normal',
+        scannedBookId: data,
+      });
+    } else if (buttonState === 'studentId') {
+      this.setState({
+        scanned: true,
+        buttonState: 'normal',
+        scannedStudentId: data,
+      });
     }
-    getCameraPermissions=async (id)=>{
-        const {status}=await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({
-            hasCameraPermissions:status=='granted',
-            buttonState:id,
-            scanned:false, 
-        })
+  };
+
+  render() {
+    const { hasCameraPermissions, scanned, buttonState, scannedBookId, scannedStudentId } = this.state;
+
+    if ((buttonState === 'bookId' || buttonState === 'studentId') && hasCameraPermissions) {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    } else if (buttonState === 'normal') {
+      return (
+        <View style={styles.container}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={styles.displayText}>Library App Scanner</Text>
+
+            <TextInput
+              style={styles.inputBox}
+              placeholder="Book ID"
+              value={scannedBookId}
+              editable={false}
+            />
+            <TouchableOpacity
+              style={styles.scanButton}
+              onPress={() => this.getCameraPermissions('bookId')}
+            >
+              <Text style={styles.buttonText}>Scan Book ID</Text>
+            </TouchableOpacity>
+
+            <TextInput
+              style={styles.inputBox}
+              placeholder="Student ID"
+              value={scannedStudentId}
+              editable={false}
+            />
+            <TouchableOpacity
+              style={styles.scanButton}
+              onPress={() => this.getCameraPermissions('studentId')}
+            >
+              <Text style={styles.buttonText}>Scan Student ID</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
     }
-    handleBarCodeScanned=async ({type,data})=>{
-        const buttonState=this.state.buttonState;
-        if(buttonState=='bookId'){
-            this.setState({
-                scanned:true,
-                buttonState:'normal',
-                scannedBookId:data,
-            })
-        }
-        else if(buttonState=='studentId'){
-            this.setState({
-                scanned:true,
-                buttonState:'normal',
-                scannedStudentId:data,
-            })
-        }
-        
-    }
-    render(){
-        const hasCameraPermissions=this.state.hasCameraPermissions;
-        const scanned=this.state.scanned;
-        const buttonState=this.state.buttonState;
-        if ((buttonState === 'bookId' || buttonState === 'studentId') && hasCameraPermissions) {
-            return(
-                <BarCodeScanner onBarCodeScanned={scanned?undefined:this.handleBarCodeScanned} 
-                style={StyleSheet.absoluteFillObject}></BarCodeScanner>
-                
-            )
-        }
-        else if(buttonState=='normal'){
-            return(
-                
-                <View style={styles.container}>
-                    <View>
-                    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                    <Text>Dummy Text</Text>
-                    </View>
-                   
-                    </View>
-                    <View >
-                       
-                       
-                     
-                 
-                    </View>
-                    
-                </View>
-            )
-        }
-        
-    }
+  }
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    displayText:{
-      fontSize: 15,
-      textDecorationLine: 'underline'
-    },
-    scanButton: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  displayText: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  inputBox: {
+    width: 200,
+    height: 40,
+    borderWidth: 1.5,
+    fontSize: 18,
+    padding: 8,
+    margin: 10,
+  },
+  scanButton: {
     backgroundColor: '#66BB6A',
     padding: 10,
     margin: 10,
-    width: 50,
-    borderWidth: 1.5,
-    borderLeftWidth: 0
-    },
-    buttonText:{
-      fontSize: 15,
-      textAlign: 'center',
-      marginTop: 10
-    },
-    
-    inputBox:{
-      width: 200,
-      height: 40,
-      borderWidth: 1.5,
-      borderRightWidth: 0,
-      fontSize: 20
-    }
-  });
+    width: 150,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
